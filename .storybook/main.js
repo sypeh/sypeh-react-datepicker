0c450d19895e9b6d90c2ae03e6139877382e7fe9
sypeh <syunpe.taro.1219@gmail.com>
2022-07-02 15:49:18 +0900

    no message

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
 module.exports = {
-    stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  }
 };
--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import renderer from 'react-test-renderer';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Default as Thing } from '../stories/Thing.stories';
 
-import { SyButton } from '../src/components/button/SyButton'
-
-configure({ adapter: new Adapter() })
-
-describe('SyButton', () => {
-  it('renders basic wrapper correctly', () => {
-    const component = renderer.create(<SyButton/>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot()
-  })
-})
+describe('Thing', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Thing />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
